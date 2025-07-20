ff67c1c495392f646670190ddbc986c46f269f3f
Andrés Reverón Molina <andressser@gmail.com>
2025-07-20 19:34:55 +0200

    Migrate to Vite Tailwind plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://andres.reveronmolina.me',
-    integrations: [tailwind()]
+  site: 'https://andres.reveronmolina.me',
+  integrations: [],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
